fccea076f8377b6528d9435e3f462c7763d16a2f
dominik_g <domin.halemb@gmail.com>
2025-07-02 17:40:37 +0200

    Modify Routes for Main Page #13

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::fallback(function () {
+    return redirect('/');
+});
+
+/**
+ * The following lines are created automatically.
+ */
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,4 +36,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';

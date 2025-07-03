050aa618986926a277a88d015036c8f553df0ca4
dominik_g <domin.halemb@gmail.com>
2025-07-03 03:18:10 +0200

    make application logo clickable by adding onclick event to redirect to homepage

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,7 @@
-<h1 class="relative text-6xl md:text-7xl font-black tracking-tight text-transparent bg-gradient-to-br from-orange-400 via-orange-500 to-orange-700 bg-clip-text drop-shadow-lg select-none animate-gradient-x">
-                    Tasklify
-                    <span class="absolute -bottom-3 left-1/2 -translate-x-1/2 w-2/3 h-3 blur-xl opacity-80 bg-gradient-to-r from-orange-400 via-orange-500 to-orange-700 pointer-events-none animate-pulse"></span>
-                </h1>
+<h1
+    class="relative text-6xl md:text-7xl font-black tracking-tight text-transparent bg-gradient-to-br from-orange-400 via-orange-500 to-orange-700 bg-clip-text drop-shadow-lg select-none animate-gradient-x cursor-pointer"
+    onclick="window.location.href='/'"
+>
+    Tasklify
+    <span class="absolute -bottom-3 left-1/2 -translate-x-1/2 w-2/3 h-3 blur-xl opacity-80 bg-gradient-to-r from-orange-400 via-orange-500 to-orange-700 pointer-events-none animate-pulse"></span>
+</h1>

be5989d266afa76c35ea101d7e9f49ac268d5aeb
dominik_g <domin.halemb@gmail.com>
2025-07-03 03:30:59 +0200

    Refactor application logo to use onclick event for homepage redirection instead of anchor tag

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,10 +1,10 @@
-<a href="/" class="group block select-none">
-    <h1
-        class="relative text-6xl md:text-7xl font-black tracking-tight text-transparent bg-gradient-to-br from-orange-400 via-orange-500 to-orange-700 bg-clip-text drop-shadow-lg animate-gradient-x cursor-pointer group-focus:outline-none group-focus:ring-2 group-focus:ring-orange-500"
-        tabindex="0"
-        aria-label="Go to homepage"
-    >
-        Tasklify
-        <span class="absolute -bottom-3 left-1/2 -translate-x-1/2 w-2/3 h-3 blur-xl opacity-80 bg-gradient-to-r from-orange-400 via-orange-500 to-orange-700 pointer-events-none animate-pulse"></span>
-    </h1>
-</a>
+<h1
+    class="relative text-6xl md:text-7xl font-black tracking-tight text-transparent bg-gradient-to-br from-orange-400 via-orange-500 to-orange-700 bg-clip-text drop-shadow-lg animate-gradient-x cursor-pointer group-focus:outline-none group-focus:ring-2 group-focus:ring-orange-500"
+    tabindex="0"
+    aria-label="Go to homepage"
+    onclick="window.location.href='{{ url('/') }}'"
+>
+    Tasklify
+    <span class="absolute -bottom-3 left-1/2 -translate-x-1/2 w-2/3 h-3 blur-xl opacity-80 bg-gradient-to-r from-orange-400 via-orange-500 to-orange-700 pointer-events-none animate-pulse"></span>
+</h1>
+

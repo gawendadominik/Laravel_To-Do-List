878b37357848f465f95d6b83b9035b6f3c1dc4bf
dominik_g <domin.halemb@gmail.com>
2025-07-06 22:51:29 +0200

    Fix overflow behavior in layout and profile edit pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased bg-gray-100 overflow-hidden">
+    <body class="font-sans antialiased bg-gray-100 overflow-auto">
         <!-- Navigation always on top -->
         <div class="min-h-screen dark:bg-gray-900">
             <div class="sticky top-0 z-50">
